fc51c6e7c4b294daaa3ae81c78ae54f490c9bb4a
Famfe4 <102044886+Famfe4@users.noreply.github.com>
2022-03-24 19:15:55 +0100

    5

---
+++ cambios.js
@@ -0,0 +1,13 @@
+let textoBoton = document.getElementById("btnCambiar").innerText;
+const clase = document.getElementById("galeria").innerText;
+
+
+function cambiarvista() {
+    if (galeria.className == "grid") {
+        galeria.className = "list";
+        textoBoton = document.getElementById("btnCambiar").innerText = "Mostrar en modo rejilla";
+    } else {
+        galeria.className = "grid";
+        textoBoton = document.getElementById("btnCambiar").innerText = "Mostrar en modo lista";
+    }
+}cambiarvista();
